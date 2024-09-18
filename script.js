16e06946174b9f1083c6225f63115175f6c6eb75
Hazel Donaldson <hazel90.hd@gmail.com>
2024-09-18 14:15:06 -0400

    implement message components

---
+++ script.js
@@ -0,0 +1,6 @@
+/* Arrays for the four components of the horoscope */
+const starSign = ['Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn', 'Aquarius', 'Pisces'];
+const retrograde = ['Moon', 'Mercury', 'Venus', 'Saturn', 'Jupiter'];
+const willExperience = ['insiatiable curiousity', 'endless joy', 'childlike wonder', 'relentless fatigue', 'deep sadness'];
+const onTheLookoutFor = ['jealous enemies', 'technology glitches', 'happy endings', 'quick closures', 'unforeseen challenges'];
+
